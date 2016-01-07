f0d4854005f9c2889e721f4091f58f27d71003d6
Jeffrey Johnson <jefjohms@gmail.com>
2016-01-07 12:27:05 -0800

    Initial Commit

---
+++ Assets/scripts/Attach.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Attach : MonoBehaviour
+{
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, -transform.up * 10);
+    }
+    
+    void FixedUpdate()
+    {
+        int layerMask = 1 << 8;//only use the Attachable layer
+        RaycastHit[] hits = Physics.RaycastAll(transform.position, -transform.up, layerMask);
+        foreach(RaycastHit hit in hits)
+        {
+            float distance = hit.distance;
+            transform.position += -transform.up * (hit.distance - .01f);
+            transform.parent = hit.transform;
+        }
+    }
+}
---
+++ Assets/scripts/Boom.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Boom : MonoBehaviour
+{
+    public float force;
+    public float radius;
+
+    public void DoIt()
+    {
+        Rigidbody rBody = transform.parent.GetComponent<Rigidbody>();
+        Debug.Log("Boom");
+        rBody.AddExplosionForce(force, transform.position, radius, 0, ForceMode.Impulse);
+        Destroy(gameObject);
+    }
+
+}
